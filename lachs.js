10d7954975970ab7dd3357170ee77dbd2bd3411a
Oleg Olanovski <oleg-olanovski@t-online.de>
2025-06-07 09:58:23 +0200

    Update add Rezept file

---
+++ lachs.js
@@ -0,0 +1,23 @@
+
+const basePortions = 4;
+const ingredients = {
+    lachs: { amount: 500, unit: "g" },
+    essig: { amount: 30, unit: "ml" },
+    zitronensaft: { amount: 30, unit: "ml" },
+    wein: {amount: 45, unit: "ml"},
+    oel: {amount: 4, unit: "EL"},
+    zucker: {amount: 4, unit: "g"},
+    kapern: {amount: 4, unit: "EL"},
+};
+
+
+document.getElementById("portionDiv").addEventListener("submit", function(event){
+    event.preventDefault();
+    const portions = parseFloat(document.getElementById("portions").value);
+    
+    for (const key in ingredients) {
+        const base = ingredients[key];
+        const result = (base.amount / basePortions) * portions;
+        document.getElementById(key).textContent = result + " " + base.unit;
+    }
+});
