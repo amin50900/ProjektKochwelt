301b80c8e927104ddaefdd85412769424a5aee04
Hannes Huspek <hannes.huspek@gmx.ch>
2025-06-15 12:10:39 +0200

    allgemeine anpassung und fertigstellung

--- lachs.js
+++ lachs.js
@@ -4,17 +4,18 @@ const ingredients = {
     lachs: { amount: 500, unit: "g" },
     essig: { amount: 30, unit: "ml" },
     zitronensaft: { amount: 30, unit: "ml" },
-    wein: {amount: 45, unit: "ml"},
-    oel: {amount: 4, unit: "EL"},
-    zucker: {amount: 4, unit: "g"},
-    kapern: {amount: 4, unit: "EL"},
+    wein: { amount: 45, unit: "ml" },
+    oel: { amount: 4, unit: "EL" },
+    zucker: { amount: 4, unit: "g" },
+    kapern: { amount: 4, unit: "EL" },
 };
 
 
-document.getElementById("portionDiv").addEventListener("submit", function(event){
+document.getElementById("portionDiv").addEventListener("submit", function (event) {
     event.preventDefault();
     const portions = parseFloat(document.getElementById("portions").value);
-    
+
+
     for (const key in ingredients) {
         const base = ingredients[key];
         const result = (base.amount / basePortions) * portions;
